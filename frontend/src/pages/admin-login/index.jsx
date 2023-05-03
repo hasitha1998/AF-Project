e2fbef230a47762f1e1d1aafe34c7fd09865675a
Sahan Punchihewa <sahannilupul18@gmail.com>
2023-05-03 23:59:22 +0530

    Admin Login UI Completed with frontend validation

---
+++ frontend/src/pages/admin-login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminLogin from "./AdminLogin";
+
+const index = () => {
+    return (
+        <div>
+            <AdminLogin />
+        </div>
+    );
+};
+
+export default index;
