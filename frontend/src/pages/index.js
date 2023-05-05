6380250b960cca809e760cc606418d6a627f701c
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2023-05-05 23:29:47 +0530

    Routes, pages, API done

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,21 @@
 import Home from "./home";
 import AdminLogin from "./admin-login";
-import AdminDashboard from "./admin-dashboard"
+import AdminDashboard from "./admin-dashboard";
 import ManageUsers from "./user-management/ManageUsers";
 import PendingAccount from "./user-management/PendingAccounts";
 import ComplaintAdd from "./complaint-add";
 import CustomerRegister from "./customer-register";
+import GovAuthLogin from "./gov-auth-login/GovAuthLogin";
+import GovAuthRegister from "./gov-auth-register/GovAuthRegister";
 
-export { Home, AdminLogin, AdminDashboard, ManageUsers, PendingAccount,ComplaintAdd, CustomerRegister};
-
+export {
+  Home,
+  AdminLogin,
+  AdminDashboard,
+  ManageUsers,
+  PendingAccount,
+  ComplaintAdd,
+  CustomerRegister,
+  GovAuthLogin,
+  GovAuthRegister,
+};
--- frontend/src/routes/app-routes.jsx
+++ frontend/src/routes/app-routes.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages
-import { Home, AdminLogin, AdminDashboard, PendingAccount,ManageUsers,ComplaintAdd, CustomerRegister} from "../pages";
-
+import {
+  Home,
+  AdminLogin,
+  AdminDashboard,
+  PendingAccount,
+  ManageUsers,
+  ComplaintAdd,
+  CustomerRegister,
+  GovAuthLogin,
+  GovAuthRegister,
+} from "../pages";
 
 // Components
 import Header from "../components/Header";
@@ -19,20 +28,20 @@ const AppRoutes = () => {
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route path="/customer/register" element={<CustomerRegister />} />
 
+          {/* Government Authority Routes */}
+          <Route path="/gov/login" element={<GovAuthLogin />} />
+          <Route path="/gov/register" element={<GovAuthRegister />} />
+
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
 
           {/* User Management Routes */}
           <Route path="/user/pending" element={<PendingAccount />} />
           <Route path="/user/userManage" element={<ManageUsers />} />
 
-
           {/*Complaint Routes*/}
           <Route path="/complaint/add" element={<ComplaintAdd />} />
 
           {/*Customer Routes*/}
-         
-
-
         </Routes>
       </Router>
     </>
