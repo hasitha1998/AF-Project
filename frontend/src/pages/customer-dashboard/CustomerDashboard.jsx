d35c23902c0f556fc5f980b2fd69e080637c5525
Shanuka Prabodha <shanukakariyawasam3@gmail.com>
2023-05-07 11:47:22 +0530

    fixes

--- frontend/src/pages/customer-dashboard/CustomerDashboard.jsx
+++ frontend/src/pages/customer-dashboard/CustomerDashboard.jsx
@@ -6,10 +6,9 @@ const CustomerDashboard = () => {
 
     return ( 
     <>
-    <Link to="/complaint/add">
-    <div className="bg-blue-400 w-36 h-36">Add Complaint</div>
-    </Link>
-
+    <div className="w-full h-full bg-blue-400">
+        hello
+    </div>
     </> 
     );
 }
